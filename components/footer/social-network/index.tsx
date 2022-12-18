7b6e8b065635a50633f86c5e628d0fdacdb34553
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-12-18 10:33:36 -0300

    feat(footer): new get for icons

--- components/footer/social-network/index.tsx
+++ components/footer/social-network/index.tsx
@@ -1,8 +1,8 @@
-import { FaInstagram, FaLinkedin, FaYoutube } from "react-icons/fa";
 import { ISocialNetworking } from "../../../interfaces/ISocialNetworking";
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import axios from "axios";
+import * as Icons from 'react-icons/fa';
 
 export default function SocialNetworking() {
   const [socialNetworking, setSocialNetworking] = useState<ISocialNetworking>();
@@ -20,13 +20,13 @@ export default function SocialNetworking() {
   return (
     <div className="flex justify-center align-middle items-center gap-4">
       <Link href="#" className="p-2 bg-[#222222] hover:bg-gray-600">
-        <FaInstagram className="text-white" />
+        <Icons.FaInstagram className="text-white" />
       </Link>
       <Link href="#" className="p-2 bg-[#222222] hover:bg-gray-600">
-        <FaLinkedin className="text-white" />
+        <Icons.FaLinkedin className="text-white" />
       </Link>
       <Link href="#" className="p-2 bg-[#222222] hover:bg-gray-600">
-        <FaYoutube className="text-white" />
+        <Icons.FaYoutube className="text-white" />
       </Link>
     </div>
   );
